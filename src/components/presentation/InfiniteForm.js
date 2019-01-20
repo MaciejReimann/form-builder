d8b376aa9a6e74c67b78165ca9d9ec563baf93b5
MaciejReimann <maciej.reimann@gmail.com>
2019-01-20 21:59:53 +0100

    minor refactor

--- src/components/presentation/InfiniteForm.js
+++ src/components/presentation/InfiniteForm.js
@@ -3,10 +3,14 @@ import React from "react";
 export default ({ onAddInput, inputs }) => {
   const onSubmit = e => e.preventDefault();
   return (
-    <div onSubmit={onSubmit}>
-      <h3>New Form</h3>
-      {inputs}
-      <button onClick={onAddInput}>Add Input</button>
-    </div>
+    <form className="InfiniteForm" onSubmit={onSubmit}>
+      <fieldset>
+        <legend>
+          <h4>Your New Form</h4>
+        </legend>
+        {inputs}
+        <button onClick={onAddInput}>Click To Add Input</button>
+      </fieldset>
+    </form>
   );
 };
